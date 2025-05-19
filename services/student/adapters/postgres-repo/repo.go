8ab6da79b46d6b5e2d3c57e04d131890e734ab4c
Michael Pochtar <delta@mac.local>
2025-05-19 16:22:37 +0200

    some sql

--- services/student/adapters/postgres-repo/repo.go
+++ services/student/adapters/postgres-repo/repo.go
@@ -2,6 +2,7 @@ package postgres_repo
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"student/ports"
@@ -14,9 +15,20 @@ type Repo struct {
 }
 
 func NewRepo() *Repo {
-	pool, err := pgxpool.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	ctx := context.Background()
+	pool, err := pgxpool.Connect(ctx, os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	sql := `CREATE TABLE IF NOT EXISTS student (
+		id TEXT PRIMARY KEY NOT NULL,
+		name TEXT NOT NULL
+	);`
+
+	_, err = pool.Exec(ctx, sql)
+	if err != nil {
+		log.Fatalf("failed to create student table")
 	}
 
 	return &Repo{pool: pool}
@@ -25,17 +37,46 @@ func NewRepo() *Repo {
 var _ ports.Repo = (*Repo)(nil)
 
 func (r *Repo) Create(student ports.Student) error {
+	sql := `INSERT INTO student ("id", "name") VALUES ($1, $2);`
+
+	_, err := r.pool.Exec(context.Background(), sql, student.Id, student.Name)
+	if err != nil {
+		return fmt.Errorf("failed to insert student: %v\n", err)
+	}
+
 	return nil
 }
 
 func (r *Repo) Get(id string) (ports.Student, error) {
-	return ports.Student{}, nil
+	sql := `SELECT * FROM student WHERE id = $1;`
+
+	var student ports.Student
+	err := r.pool.QueryRow(context.Background(), sql, id).Scan(&student.Id, &student.Name)
+	if err != nil {
+		return student, fmt.Errorf("failed to query student %s: %v\n", student.Id, err)
+	}
+
+	return student, nil
 }
 
 func (r *Repo) Update(student ports.Student) error {
+	sql := `UPDATE student SET name = $1 WHERE id = $2;`
+
+	_, err := r.pool.Exec(context.Background(), sql, student.Name, student.Id)
+	if err != nil {
+		return fmt.Errorf("failed to update student %s: %v\n", student.Id, err)
+	}
+
 	return nil
 }
 
 func (r *Repo) Delete(id string) error {
+	sql := `DELETE student WHERE id = $1;`
+
+	_, err := r.pool.Exec(context.Background(), sql, id)
+	if err != nil {
+		return fmt.Errorf("failed to delete student %s: %v", id, err)
+	}
+
 	return nil
 }
